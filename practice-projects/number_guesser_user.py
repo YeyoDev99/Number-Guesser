d5e0adee680093f1181a01153ffbbc23c00c097c
YeyoDev99 <yeyoduglas2004@gmail.com>
2022-05-26 19:51:59 -0500

    penultimo testing antes de la primera versión del programa

--- practice-projects/number_guesser_user.py
+++ practice-projects/number_guesser_user.py
@@ -1,14 +1,28 @@
 class User():
-    def __init__(self, max_score = 0, best_time=0,level = 0):
-        self.max_score = max_score
-        self.best_time = best_time
-        self.level = level
+    def __init__(self):
+        with open("./max-score.txt", "r", encoding="UTF-8") as max_score_sheet:
+            for _ in max_score_sheet:
+                max_score_user = int(_)
+                self.max_score = max_score_user
+        with open("./best-time.txt", "r", encoding="UTF-8") as best_time_sheet:
+            for _ in best_time_sheet:
+                best_time_user = int(_)
+                self.best_time = best_time_user 
+        with open("./level.txt", "r", encoding="UTF-8") as level_sheet:
+            for _ in level_sheet:
+                level_user = int(_)
+                self.level = level_user
+        with open("./number_of_wins.txt", "r", encoding="UTF-8") as num_wins_user_sheet:
+            for _ in num_wins_user_sheet:
+                num_wins_user_value = int(_)
+                self.wins = num_wins_user_value
     def new_max_score(self, new_max_user_score):
         self.max_score = new_max_user_score
     def new_best_time(self, new_best_user_time):
         self.best_time = new_best_user_time
     def new_level(self,new_user_level):
-        self.level = new_user_level       
-
+        self.level = new_user_level  
+    def total_wins(self,new_number_of_wins):
+        self.wins = new_number_of_wins         
 
 
--- practice-projects/python_test1.py
+++ practice-projects/python_test1.py
@@ -6,7 +6,31 @@ from number_guesser_user import User
 
 def game(game_mode):
         while True:
-            game_user = User(0,0,0)
+            game_user = User()
+            if game_mode == 5:
+                while True:
+                    os.system('cls')
+                    print("Estadisticas de adivinador".upper())
+                    print("\n------------------------------------")
+                    print("Nivel:",          str(game_user.level))
+                    print("------------------------------------")
+                    print("Puntaje maximo:", str(game_user.max_score))
+                    print("------------------------------------")
+                    print("Mejor tiempo:",   str(game_user.best_time), "segundos")
+                    print("------------------------------------")
+                    print("Victorias:",      str(game_user.wins))
+                    print("------------------------------------")
+                    stadistics_desicion = input("\nDigita la letra -y- para volver al menu, escribe cualquier otra cosa para salir del programa: ").replace(" ","").lower()
+                    os.system('cls')
+                    if stadistics_desicion != "y":
+                        break
+                    if stadistics_desicion == "y":
+                        game_mode = int(input(MENU).replace(" ",""))
+                        os.system('cls')
+                        if game_mode != 5:
+                            break
+            if game_mode == 5:
+                break            
             start_time = datetime.utcnow()
             if game_mode == 1:
                 limite = 51
@@ -22,12 +46,12 @@ def game(game_mode):
                 limite = 101
                 lives = 6
                 mode = "dificil"
-                points_base = 300
+                points_base = 600
             if game_mode == 4:
                 limite = 201
                 lives = 6     
                 mode = "muy dificil"
-                points_base = 700
+                points_base = 1400
             cpu_number = random.randint(1,limite)
             print("Has elegido el modo", mode, "por lo tanto tienes", str(lives-1), "vidas y la cpu ha elegido un numero del 1 al", str(limite-1))                
             user_election = int(input("elige tu primer numero: ").replace(" ",""))
@@ -47,26 +71,38 @@ def game(game_mode):
                 if lives == 0:
                     break              
             if user_election == cpu_number:
+                with open("./number_of_wins.txt", "r",encoding="UTF-8") as nnowbu:
+                    for num in nnowbu:
+                        number_of_guessed_wins = int(num)
+                        with open("./number_of_wins.txt", "w", encoding="UTF-8") as nnowbuwa:
+                            nnowbuwa.write(str(number_of_guessed_wins + 1))
+                            game_user.total_wins(number_of_guessed_wins + 1)
+                            break
                 os.system('cls')
+                print("------------------------------------")
                 print("el numero de la CPU es", str(cpu_number))
+                print("------------------------------------")
                 time.sleep(2)
                 end_time = datetime.utcnow()
                 total_time = end_time - start_time
                 total_time = round(total_time.total_seconds(),None)
                 total_points = round(points_base * 10000 / total_time,None)
                 print("felicidades, has obtenido", str(total_points), "puntos")
+                print("------------------------------------")
                 time.sleep(2)
                 with open("./max-score.txt", "r", encoding="UTF-8") as ms:
                     for i in  ms:
                         if total_points > int(i):
                             game_user.new_max_score(total_points)
                             print("Enorabuena!, has conseguido un nuevo puntaje maximo")
+                            print("------------------------------------")
                             with open("./max-score.txt", "w", encoding="UTF=8") as nms:
                                 nms.write(str(total_points))
                             time.sleep(2)
                             break    
                         if total_points <= int(i):
-                            print("tu puntaje maximo es de", str(i), ",intenta superarlo la proxima vez")
+                            print("tu puntaje maximo es de", str(i), "de puntos, intenta superarlo la proxima vez")
+                            print("------------------------------------")
                             time.sleep(2)
                             break
                 with open("./user_points.txt", "r", encoding="UTF=8") as up:
@@ -81,6 +117,7 @@ def game(game_mode):
                                         with open("./level.txt", "w", encoding="UTF-8") as nulig:
                                             nulig.write(str(int(i)+1))
                                             print("Increible!, has subido a nivel", str(int(i)+1),",eres todo un adivinador!")
+                                            print("------------------------------------")
                                             time.sleep(3.5)
                                             with open("./level.txt", "r", encoding="UTF-8") as frfi:
                                                 for _ in frfi:
@@ -88,26 +125,30 @@ def game(game_mode):
                                     else:
                                         time.sleep(1)
                                         print("necesitas", str(limit_level -new_user_points_saved), "puntos más para subir a nivel", str(int(i)+1))
+                                        print("------------------------------------")
                                         time.sleep(3)
                                         break       
                             break                 
                 print("tu partida a durado", str(total_time), "segundos")
+                print("------------------------------------")
                 with open("./best-time.txt", "r", encoding="UTF-8") as bt:
                     for i in  bt:
                         if total_time < int(i):
                             game_user.new_best_time(total_time)
                             time.sleep(2)
                             print("Enorabuena!, has conseguido un nuevo mejor tiempo de partida")
+                            print("------------------------------------")
                             with open("./best-time.txt", "w", encoding="UTF=8") as nbt:
                                 nbt.write(str(total_time))
                             break    
                         if total_time >= int(i):
                             time.sleep(2)
                             print("tu mejor tiempo es de", str(i), "segundos, intenta mejorarlo la proxima vez")
+                            print("------------------------------------")
                             break 
                 time.sleep(2)
                 game_mode = input(MENU2).replace(" ","")
-                if game_mode.isalpha() or int(game_mode) > 4 or int(game_mode) < 1:
+                if game_mode.isalpha() or int(game_mode) > 5 or int(game_mode) < 1:
                     print("gracias por jugar!")
                     time.sleep(3)
                     break
@@ -115,18 +156,22 @@ def game(game_mode):
                 os.system('cls')        
             elif lives == 0:
                 os.system('cls')
+                print("------------------------------------")
                 print("el numero de la CPU era", str(cpu_number))
+                print("------------------------------------")
                 time.sleep(2)
                 end_time = datetime.utcnow()
                 total_time = end_time - start_time
                 total_time = round(total_time.total_seconds(),None)
                 total_points = round(points_base * 10000 / total_time,None)
                 print("lo siento, has perdido y por lo tanto has obtenido 0 puntos")
+                print("------------------------------------")
                 time.sleep(1)
                 print("gracias por jugar, tu partida a durado", str(total_time), "segundos")
+                print("------------------------------------")
                 time.sleep(2)
                 game_mode = input(MENU3).replace(" ","")
-                if game_mode.isalpha() or int(game_mode) > 4 or int(game_mode) < 1:
+                if game_mode.isalpha() or int(game_mode) > 5 or int(game_mode) < 1:
                     print("gracias por jugar!")
                     time.sleep(3)
                     break
@@ -163,6 +208,9 @@ solo puedes digitar los numeros:
 2-normal
 3-dificil
 4-muy dificil
+-------------------------------------------
+5-estadisticas
+-------------------------------------------
 
 si quieres continuar con el programa escribe la letra -y-, escribe cualquier otra cosa para salir: """
 
@@ -176,6 +224,8 @@ si quieres seguir jugando escribe el numero de la dificultad en que quieres juga
 2-normal
 3-dificil
 4-muy dificil
+
+o accede a tus estadisticas con el numero 5
                                          
 escribe cualquier otra cosa para salir del programa: """
 
@@ -189,6 +239,8 @@ si quieres seguir jugando escribe el numero de la dificultad en que quieres juga
 2-normal
 3-dificil
 4-muy dificil
+
+o accede a tus estadisticas con el numero 5
                                          
 escribe cualquier otra cosa para salir del programa: """
 
@@ -215,8 +267,15 @@ Elige tu dificultad:
 :Entre mayor sea la dificultad y entre menor sea el tiempo que tardes en adivinar el numero 
 :Mayor sera tu puntaje
 
+----------------------------------------------------------------------
+
+:Puedes digitar el numero 5 para ver tus estadisticas
+
+----------------------------------------------------------------------
+
 digita el numero de la dificultad en la que deseas jugar
-(1,2,3,4): """
+
+(1,2,3,4) o accede a tus estadisticas(5): """
 
 
 
